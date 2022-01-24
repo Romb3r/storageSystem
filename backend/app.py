329ebaa9981ec3f68c86168bd8f8421ebbd4c979
Romb3r <80882627+Romb3r@users.noreply.github.com>
2022-01-24 14:19:56 +0100

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -4,38 +4,47 @@ import db, codecs
 
 app = FastAPI()
 
+
 def generate_html_res():
     html_content = codecs.open("../frontend/index.html", "r", encoding="utf-8")
     return HTMLResponse(content=html_content.read(), status_code=200)
+
 
 @app.get("/index.html", response_class=HTMLResponse)
 def print_service():
     return generate_html_res()
 
+
 @app.get("/get/all/food")
 def get_all_food():
     return db.get_all_food()
 
+
 @app.get("/get/food/{typ}")
 def get_food(typ: str):
     return db.get_food(typ)
+
 
 @app.post("/insert/food/{typ}/{amount}/{place}")
 def insert_food(typ: str, amount: int, place: str):
     db.insert_food(typ, amount, place)
     return {"Typ": typ, "Menge": amount, "Ort": place}
 
-@app.put("/update/food/{typ}/{operation}")
-def update_food_amount(typ: str, operation: bool):
-    amount = db.update_food_amount(typ, operation)
+
+@app.put("/update/food/{typ}/{amount}")
+def update_food_amount(typ: str, amount: int):
+    amount = db.update_food_amount(typ, amount)
     if amount == 0:
+        db.delete_food(typ)
         return {"Typ": typ, "Status": "deleted"}
     return {"Typ": typ, "Neue Menge": amount}
+
 
 @app.delete("/delete/food/{typ}")
 def delete_food(typ: str):
     db.delete_food(typ)
     return {"Typ": typ, "Status": "deleted"}
+
 
 @app.delete("/delete/all/food")
 def delete_all_food():
--- backend/db.py
+++ backend/db.py
@@ -1,9 +1,7 @@
 import sqlite3
 
 conn = sqlite3.connect('items.db', check_same_thread=False)
-
 c = conn.cursor()
-
 
 
 def insert_food(typ, amount, place):
@@ -16,22 +14,12 @@ def insert_food(typ, amount, place):
     with conn:
         c.execute("INSERT INTO food VALUES (:Ftype, :amount, :place)", {'Ftype': typ, 'amount': amount, 'place': place})
 
-def update_food_amount(typ, operation):
-    c.execute("SELECT amount FROM food WHERE Ftype = :Ftype", {'Ftype': typ})
-    amount = c.fetchone()
-    if amount[0] - 1 == 0:
-        delete_food(typ)
-        return amount[0] - 1
-    with conn:
-        if operation:
-            c.execute("""UPDATE food SET amount = :amount
-                        WHERE Ftype = :Ftype""", {'Ftype': typ, 'amount': amount[0] + 1})
-            return amount[0]  + 1
 
-        else:
-            c.execute("""UPDATE food SET amount = :amount
-                                    WHERE Ftype = :Ftype""", {'Ftype': typ, 'amount': amount[0] - 1})
-            return amount[0] - 1
+def update_food_amount(typ, amount):
+    c.execute("""UPDATE food SET amount = :amount
+                WHERE Ftype = :Ftype""", {'Ftype': typ, 'amount': amount})
+    return amount
+
 
 def get_all_food():
     arrayData = []
@@ -42,16 +30,19 @@ def get_all_food():
     arrayDictData = [{"Typ": item[0], "Menge": item[1], "Ort": item[2]} for item in arrayData]
     return arrayDictData
 
+
 def get_food(typ):
     c.execute("SELECT * FROM food WHERE Ftype = :Ftype", {'Ftype': typ})
     data = c.fetchone()
     dictData = [{"Typ": data[0], "Menge": data[1], "Ort": data[2]}]
     return dictData
 
+
 def delete_food(typ):
     with conn:
         c.execute("DELETE FROM food WHERE Ftype = :Ftype",
                   {'Ftype': typ})
+
 
 def delete_all_food():
     with conn:
